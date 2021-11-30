2380767037a0a2a693a8c4a3e9012848fcdeb6ad
Victor Castañeda <vidanilo10@gmail.com>
2021-11-30 16:15:27 -0500

    Execute typescript file

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let edad = 33;
+let nombre = 'Daniel';
+console.log(nombre);
+console.log(edad);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+let edad: number = 33
+let nombre: string = 'Daniel'
+
+console.log(nombre)
+console.log(edad)
